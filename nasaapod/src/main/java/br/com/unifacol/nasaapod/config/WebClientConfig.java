2f14ecb0860f265ac8eb2bc841a1a2da66577e36
M4theus1 <matheus.silva.alves2909@gmail.com>
2025-11-10 20:28:11 -0300

    timeout exceptions

--- nasaapod/src/main/java/br/com/unifacol/nasaapod/config/WebClientConfig.java
+++ nasaapod/src/main/java/br/com/unifacol/nasaapod/config/WebClientConfig.java
@@ -2,14 +2,24 @@ package br.com.unifacol.nasaapod.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import io.netty.channel.ChannelOption;
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
+
     @Bean
-    public WebClient webClient(WebClient.Builder builder) {
-        return builder.baseUrl("https://api.nasa.gov/planetary").build();
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl("https://api.nasa.gov")
+                .clientConnector(new ReactorClientHttpConnector(
+                        HttpClient.create()
+                                .responseTimeout(Duration.ofSeconds(30))
+
+                ))
+                .build();
     }
 }
-
-
